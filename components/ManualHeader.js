e07329336eb196f60a899c6b505dac2004afd922
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-18 14:40:30 -0600

    Display connected address to user

--- components/ManualHeader.js
+++ components/ManualHeader.js
@@ -1,11 +1,17 @@
 import { useMoralis } from "react-moralis"
 
 export default function ManualHeader() {
-    const { enableWeb3 } = useMoralis()
+    const { enableWeb3, account } = useMoralis()
 
     return (
         <>
-            <button onClick={async () => await enableWeb3}>Connect</button>
+            {account ? (
+                <div>
+                    Connected to: {account.slice(0, 6)}...{account.slice(account.length - 4)}
+                </div>
+            ) : (
+                <button onClick={async () => await enableWeb3()}>Connect</button>
+            )}
         </>
     )
 }
