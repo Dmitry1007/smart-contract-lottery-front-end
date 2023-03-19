b18b82c2744b4d4df8596981deee2f31baca775d
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-18 19:11:36 -0600

    Toggle user connected button, save user connection status to local storage

--- components/ManualHeader.js
+++ components/ManualHeader.js
@@ -1,7 +1,28 @@
 import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
 
 export default function ManualHeader() {
-    const { enableWeb3, account } = useMoralis()
+    const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3 } = useMoralis()
+
+    useEffect(() => {
+        if (isWeb3Enabled) return
+        if (typeof window !== "undefined") {
+            if (window.localStorage.getItem("connected")) {
+                enableWeb3()
+            }
+        }
+    }, [isWeb3Enabled])
+
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+            console.log(`Account changed to ${account}`)
+            if (account == null) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("null account found")
+            }
+        })
+    }, [])
 
     return (
         <>
@@ -10,7 +31,14 @@ export default function ManualHeader() {
                     Connected to: {account.slice(0, 6)}...{account.slice(account.length - 4)}
                 </div>
             ) : (
-                <button onClick={async () => await enableWeb3()}>Connect</button>
+                <button
+                    onClick={async () => {
+                        await enableWeb3()
+                        window.localStorage.setItem("connected", "injected")
+                    }}
+                >
+                    Connect
+                </button>
             )}
         </>
     )
