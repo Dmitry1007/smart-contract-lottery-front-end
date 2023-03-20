5fc5d541af2dd0de6474c8cfd31e76c0c9852dd4
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-20 09:30:32 -0600

    Get entrance fee from the contract

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,13 +1,37 @@
+import { useEffect } from "react"
 import { useWeb3Contract } from "react-moralis"
+import { useMoralis } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
 
 export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const lotteryAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+
     // const { runContractFunction: enterLottery } = useWeb3Contract({
-    //     abi: ,
-    //     contractAddress: ,
-    //     functionName: ,
+    //     abi: abi,
+    //     contractAddress: lotteryAddress,
+    //     functionName: "enterLottery" ,
     //     params: {},
     //     msgValue: ,
     // })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            async function updateUI() {
+                const entranceFeeFromContract = await getEntranceFee()
+                console.log("entranceFee ", entranceFeeFromContract)
+            }
+            updateUI()
+        }
+    }, [isWeb3Enabled])
 
     return <>Lottery Entrance</>
 }
