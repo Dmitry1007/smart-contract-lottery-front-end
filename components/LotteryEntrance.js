12d18c1a6bfe2b8261a22c8955195b049fbbacfa
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-20 11:36:00 -0600

    Display notification for successful transactions

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -3,13 +3,15 @@ import { useWeb3Contract } from "react-moralis"
 import { useMoralis } from "react-moralis"
 import { abi, contractAddresses } from "../constants"
 import { ethers } from "ethers"
+import { useNotification } from "web3uikit"
 
 export default function LotteryEntrance() {
     const [entranceFee, setEntranceFee] = useState("0")
-
     const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
     const chainId = parseInt(chainIdHex)
     const lotteryAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+
+    const dispatch = useNotification()
 
     const { runContractFunction: enterLottery } = useWeb3Contract({
         abi: abi,
@@ -36,6 +38,21 @@ export default function LotteryEntrance() {
         }
     }, [isWeb3Enabled])
 
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+    }
+
+    const handleNewNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete!",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+
     return (
         <>
             {lotteryAddress ? (
@@ -43,7 +60,10 @@ export default function LotteryEntrance() {
                     {" "}
                     <button
                         onClick={async function () {
-                            await enterLottery()
+                            await enterLottery({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.log(error),
+                            })
                         }}
                     >
                         Enter Lottery
